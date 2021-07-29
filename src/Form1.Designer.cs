c9d8da07b6734751214b660f0246d031b9b4c3fe
Veler Jevsky <43819843+Valetron@users.noreply.github.com>
2021-07-29 13:38:28 +0300

    Add files via upload

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Gla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Image (*.BMP;*.JPG;*.JPEG;*.GIF;*.PNG)|*.BMP;**.JPG;*.JPEG;*.GIF;*.PNG|All files " +
+    "(*.*)|*.*";
+            this.openFileDialog1.Title = "Выбрать изображение";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(758, 529);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseWheel);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.pictureBox1);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Glance";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Gla
+{
+    public partial class Form1 : Form
+    {
+        private int curIndexImage = 0;
+        private string[] files;
+        private static string[] extensions = { "jpg", "jpeg", "png", "gif", "bmp" };
+        private int coordX;
+        private int coordY;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (ModifierKeys == Keys.Shift && e.Delta == 120) // zoom in
+            {
+                pictureBox1.Size = new Size(pictureBox1.Width + 15, pictureBox1.Height + 15);
+                pictureBox1.Left = (this.ClientSize.Width - pictureBox1.Width) / 2;
+                pictureBox1.Top = (this.ClientSize.Height - pictureBox1.Height) / 2;
+            }
+            else if (ModifierKeys == Keys.Shift && e.Delta == -120) // zoom out
+            {
+                pictureBox1.Size = new Size(pictureBox1.Width - 15, pictureBox1.Height - 15);
+                pictureBox1.Left = (this.ClientSize.Width - pictureBox1.Width) / 2;
+                pictureBox1.Top = (this.ClientSize.Height - pictureBox1.Height) / 2;
+            }
+            else if (ModifierKeys == Keys.Control && e.Delta == 120 && pictureBox1.Image != null) // show next
+            {
+                if (curIndexImage == files.Length - 1) { curIndexImage = 0; }
+                else { curIndexImage++; }
+                pictureBox1.Load(files[curIndexImage]);
+                this.Text = "Glance at " + files[curIndexImage];
+            }
+            else if (ModifierKeys == Keys.Control && e.Delta == -120 && pictureBox1.Image != null) // show prev
+            {
+                if (curIndexImage == 0) { curIndexImage = files.Length - 1; }
+                else { curIndexImage--; }
+                pictureBox1.Load(files[curIndexImage]);
+                this.Text = "Glance at " + files[curIndexImage];
+            }
+            else if (ModifierKeys == Keys.Alt && e.Delta == 120) // rotate forward
+            {
+
+            }
+            else if (ModifierKeys == Keys.Alt && e.Delta == -120) // rotate backward
+            {
+
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right && openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    files = GetFiles(Path.GetDirectoryName(openFileDialog1.FileName));
+                    pictureBox1.Load(openFileDialog1.FileName);
+                    this.Text = "Glance at " + Path.GetFullPath(openFileDialog1.FileName);
+                }
+                catch
+                {
+                    pictureBox1.Image = null;
+                    this.Text = "Glance";
+                    MessageBox.Show("Неверный формат файла.");
+                }
+            }
+        }
+
+        private static String[] GetFiles(String folder)
+        {
+            List<String> images = new List<String>();
+            foreach (var filter in extensions)
+            {
+                images.AddRange(Directory.GetFiles(folder, String.Format("*.{0}", filter)));
+            }
+            return images.ToArray();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Width = this.ClientSize.Width;
+            pictureBox1.Height = this.ClientSize.Height;
+            var args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                var img = args[args.Length - 1];
+                if (File.Exists(img))
+                {
+                    pictureBox1.Load(img);
+                    this.Text = "Glance at " + Path.GetFullPath(img);
+                    files = GetFiles(Path.GetDirectoryName(img));
+                    foreach (var i in files)
+                    {
+                        if (i != Path.GetFullPath(img)) { curIndexImage++; }
+                    }
+                }
+            }
+            else { files = GetFiles(Directory.GetCurrentDirectory()); }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            pictureBox1.Width = this.ClientSize.Width;
+            pictureBox1.Height = this.ClientSize.Height;
+            pictureBox1.Left = (this.ClientSize.Width - pictureBox1.Width) / 2;
+            pictureBox1.Top = (this.ClientSize.Height - pictureBox1.Height) / 2;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            coordX = e.X;
+            coordY = e.Y;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + e.X - coordX, pictureBox1.Location.Y + e.Y - coordY);
+            }
+        }
+    }
+}
